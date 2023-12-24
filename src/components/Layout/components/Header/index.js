a0c1b6cb3e3bde6941cb0fc5c1e6206223c4b96e
VanTinh102 <tinhkun56@gmail.com>
2023-12-25 01:30:53 +0700

    Second commit 25/12/2023

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,21 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/static/images/Logo';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>Header{/* Logo... */}</div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className="clear">{/* clear */}</button>
+                    {/* icon loading */}
+                    <button className={cx('search-btn')}>{/* icon search */}</button>
+                </div>
+            </div>
         </header>
     );
 }
